7d438e0e5b049993f3858376fed37f7f57c171d0
anshulp-web <anshulp.dev@gmail.com>
2023-08-09 02:31:24 +0530

    frontend-project-setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+
+function App() {
+  return (
+    <>
+      <div>
+        <header>
+          <a href="/">amazona</a>
+        </header>
+        <main>list product</main>
+      </div>
+    </>
+  );
+}
+
+export default App;
